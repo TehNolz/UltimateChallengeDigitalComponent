17e6682b22f908bdf2f9c517f5a0585f6e873935
Niels <36479504+TehNolz@users.noreply.github.com>
2018-12-11 23:03:25 +0100

    Add WIP deck selection to game setup screen

--- gameSetupScreen.py
+++ gameSetupScreen.py
@@ -12,6 +12,9 @@ def init():
     global playerCount
     global addPlayerButton
     global remPlayerButton
+    global baseToggle
+    global exp1Toggle
+    global exp2Toggle
     playerCount = 4
     
     Object.startGroup()
@@ -23,25 +26,45 @@ def init():
     playButton = Button(width*0.9, height*0.8, r.copy())
     playButton.releaseAction = startGame
     playButton.text = "Play"
+    
+    baseToggle = Button(width*0.8, height*0.18, r.copy()*0.2)
+    baseToggle.applyStyle('checkbox')
+    baseToggle.boxColor = color(0,0,0, 255)
+    baseToggle.releaseAction = toggleBase
+    if "base" in globals.userConfig["settings"]["useDecks"]:
+        baseToggle.activated = True
+    
+    exp1Toggle = Button(width*0.8, height*0.28, r.copy()*0.2)
+    exp1Toggle.applyStyle('checkbox')
+    exp1Toggle.boxColor = color(0,0,0, 255)
+    exp1Toggle.releaseAction = toggleExp1
+    if "expansion1" in globals.userConfig["settings"]["useDecks"]:
+        exp1Toggle.activated = True
+    
+    exp2Toggle = Button(width*0.8, height*0.38, r.copy()*0.2)
+    exp2Toggle.applyStyle('checkbox')
+    exp2Toggle.boxColor = color(0,0,0, 255)
+    exp2Toggle.releaseAction = toggleExp2
+    if "expansion2" in globals.userConfig["settings"]["useDecks"]:
+        exp2Toggle.activated = True
+    
     buttons = Object.endGroup()
     
     #Add player
-    r = RoundRect(-100, -75, 200, 150, 25)
+    r = RoundRect(-100, -65, 200, 130, 25)
     r *= 0.5
     addPlayerButton = Button(width*0.1, height*0.2, r.copy())
     addPlayerButton.releaseAction = addPlayer
     addPlayerButton.text = "Add \nplayer."
-    addPlayerButton.textSize = 40
+    addPlayerButton.textSize = 20
     
     #Remove player
     remPlayerButton = Button(width*0.2, height*0.2, r.copy())
     remPlayerButton.releaseAction = remPlayer
     remPlayerButton.text = "Remove \nplayer."
+    remPlayerButton.textSize = 20
 
     #Create textboxes
-    remPlayerButton.text = "-"
-    remPlayerButton.textSize = 40
-    
     boxHeight = 0.7
     var = 1
     globals.textBoxDict["gameSetupScreen"] = []
@@ -51,7 +74,7 @@ def init():
         var+=1
         globals.textBoxDict["gameSetupScreen"].append(textBox)
         boxHeight -= 0.1
-    
+            
 def draw():
     global playerCount
     
@@ -73,6 +96,7 @@ def draw():
     #Header text
     textSize(40*baseX)
     text("Enter your name!", width*0.01, height*0.1)
+    text("Use which decks?", width*0.5, height*0.1)
     
     #Add text boxes + text
     textSize(30*baseX)
@@ -82,9 +106,18 @@ def draw():
         text("Player "+str(player+1), width*0.01, height*(1-textHeight))
         textHeight-=0.1
         textBox.draw()
+    
+    text("Base Deck", width*0.5, height*0.2)
+    text("Expansion Deck 1", width*0.5, height*0.3)
+    text("Expansion Deck 2", width*0.5, height*0.4)
+    
     popStyle()
     
 def startGame(*args):
+    global baseToggle
+    global exp1Toggle
+    global exp2Toggle
+    
     if args[1] == LEFT:
         if len(globals.userConfig["settings"]["useDecks"]) > 0:
             for player in range(0, playerCount):
@@ -93,6 +126,8 @@ def startGame(*args):
             
             data.saveData()
             globals.currentMenu = "gameScreen"
+        else:
+            pass
         
 #Add/remove players.
 def addPlayer(*args):
@@ -114,8 +149,12 @@ def toggleExp2(*args):
     
 def toggleDeck(deck):
     useDecks = globals.userConfig["settings"]["useDecks"]
-    if len(useDecks) > 1:
-        if deck in useDecks:
-            useDecks.remove(deck)
-        else:
-            useDecks.append(deck)
+    print(useDecks)
+    if deck in useDecks:
+        print("rem")
+        useDecks.remove(deck)
+    else:
+        print("add")
+        useDecks.append(deck)
+    print(useDecks)
+    globals.userConfig["settings"]["useDecks"] = useDecks
