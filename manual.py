342960ba3c55519de75adc78990d2382b1d3abf9
Daan de Heer <Daaandeheer@gmail.com>
2018-12-12 18:12:00 +0100

    hij geeft een error die ik niet begrijp

--- manual.py
+++ manual.py
@@ -4,7 +4,40 @@ from Button import Button
 from Object import Object
 
 def init():
-    pass
+    global manual1, manual2
+    
+    manual1 = loadImage("manual-1.png")
+    manual2 = loadImage("manual-2.png")
+    
+
+button_x = 165
+button_y = 525
+button_w = 50
+button_h = 50
+
+
+
+
+
 
 def draw():
-    pass
+    global manual1, manual2, button_x, button_y, button_w, button_h
+    
+    
+    
+    if mousePressed and (mouseButton == LEFT) and (mouseX > button_x and button_x + button_w > mouseX) and (mouseY > button_y and button_y + button_h > mouseY):
+        fill(255,0,0)
+        rect(button_x, button_y, button_w, button_h)
+    
+    
+    
+    rect(button_x, button_y, button_w, button_h)
+    
+    
+    
+    image(manual1, width/3, height/2)
+    manual1.resize(350,500)
+    
+    #image(manual2, width/2 , height/2)
+    #manual2.resize(350,500)
+    
