fead560a1768e40c1a948c34bfc9536b1bcb5882
Daan de Heer <Daaandeheer@gmail.com>
2018-12-15 19:26:05 +0100

    t werkt punt.

--- manual.py
+++ manual.py
@@ -3,12 +3,13 @@ from util import *
 from Button import Button
 from Object import Object
 
-button_x = 165
+button_x = 160
 button_y = 525
 button_x1 = 975
 button_y1 = 525
 forward = 0
 backward = 0
+
 def init():
     global manual1, manual2, manual3, manual4, manual5, manual6,  buttons, forward, backward
     
@@ -35,71 +36,87 @@ def draw(mousePressed):
     global manual1, manual2, manual3, manual4, manual5, manual6, button_x, button_y, button_w, button_h, forward, backward
     
     
-
+    textSize(50)
+    text("The Ultimate Challenge manual", width/6, 40)
+    
     
     image(manual1, width*(1.0/3), height/2)
-    manual1.resize(350,500)
-    
+    manual1.resize(370,500)
     image(manual2, width*(2.0/3), height/2)
-    manual2.resize(350,500)
-    
-    
+    manual2.resize(370,500)
+        
     if forward == 1:
         image(manual3, width*(1.0/3), height/2)
-        manual3.resize(350,500)
+        manual3.resize(370,500)
         image(manual4, width*(2.0/3), height/2)
-        manual4.resize(350,500)
+        manual4.resize(370,500)
+    
     if forward == 2:
         image(manual5, width*(1.0/3), height/2)
-        manual5.resize(350,500)
+        manual5.resize(370,500)
         image(manual6, width*(2.0/3), height/2)
-        manual6.resize(350,500)
+        manual6.resize(370,500)
+    
     if backward == 1:
         image(manual1, width*(1.0/3), height/2)
-        manual1.resize(350,500)
+        manual1.resize(370,500)
         image(manual2, width*(2.0/3), height/2)
-        manual2.resize(350,500)
-    
-    
-    
-    
-    
-    #image(manual4, width*(1.0/3), height/2)
-    #manual4.resize(350,500)
-    
-    #image(manual5, width*(1.0/3), height/2)
-    #manual5.resize(350,500)
-    
-    #image(manual6, width*(1.0/3), height/2)
-    #manual6.resize(350,500)
-    
-    
+        manual2.resize(370,500)
+        
+    if backward == 2:
+        image(manual3, width*(1.0/3), height/2)
+        manual3.resize(370,500)
+        image(manual4, width*(2.0/3), height/2)
+        manual4.resize(370,500)
+
+            
     
     for o in buttons:
         o.update()
 
 
+def pageForward(*args):
+    global forward, backward
+    if forward == 0:
+        forward = 1
+        backward = 0
+        
+    elif forward == 1:
+        forward = 2
 
+    elif forward == 2:
+        pass
+                    
 def pageBack(*args):
-    global backward
+    global backward, forward
     if backward == 0:
         backward = 1
+        forward = 0
+        
     elif backward == 1:
-        backward = 2
-        forward = 1
-    elif backward == 2:
         pass
+       
+
+    
         
     
     
-def pageForward(*args):
-    global forward
-    if forward == 0:
-        forward = 1
-    elif forward == 1:
-        forward = 2
-    elif forward == 2:
-        pass
+
+    
+    
+    
+        
+        
+    
+        
+        
+        
+
+            
+        
+    
+    
+
         
         
         
