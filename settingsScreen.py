d39f53034526c768982e6e62dbf10d03b45b5ec5
Spaalone_Babuguuscooties <0965749@hr.nl>
2018-12-18 18:36:20 +0100

    Work in progress

--- settingsScreen.py
+++ settingsScreen.py
@@ -4,22 +4,39 @@ from Button import Button
 from Object import Object
 
 def init():
+    global buttons, categories
+    
+    categories = dict()
     
     Object.startGroup()
-    
-    r = RoundRect(-25, -25, 50, 50)
-    
-    backButton = Button(width*0.95, height*0.1, r.copy())
+
+    backButton = Button(37, 37, RoundRect(-25, -25, 50, 50) * 0.75)
     backButton.releaseAction = gotoMainMenu
-    backButton.text = "Back"
+    backButton.textSize *= 3
+    backButton.applyStyle('compact')
+    backButton.description = 'Main menu'
+    backButton.icon = globals.imgIndex['back'].copy()
+    backButton.iconScale = 0.75
+    backButton.iconColor = color(0,0)
+    
+    backButton = Button(37, 37 + 67, RoundRect(-25, -25, 50, 50) * 0.75)
+    backButton.releaseAction = gotoMainMenu
+    backButton.textSize *= 3
+    backButton.applyStyle('compact')
+    backButton.description = 'Quit'
+    backButton.icon = globals.imgIndex['kruisje'].copy()
+    backButton.iconScale = 0.5
+    backButton.iconColor = color(0,0)
     
     buttons = Object.endGroup()
-    
 
 def draw():
-    pass
+    width = 1133
+    height = 600
+    scale(*globals.baseScaleXY)
     
-    
+    for o in buttons:
+        o.update()
     
 def gotoMainMenu(*args):
     if args[1] == LEFT:
