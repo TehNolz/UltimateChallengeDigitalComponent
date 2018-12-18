afc37be165bfe5245d38d486ac1a77fa5b18aaa4
Spaalone_Babuguuscooties <0965749@hr.nl>
2018-12-18 18:36:39 +0100

    Code cleanup

--- test.py
+++ test.py
@@ -17,65 +17,38 @@ def init():
     b = Button(150,300,r.copy()*0.5)
     b.applyStyle('compact')
     b.text = 'test\nbutton'
-    b.description = 'placeholder\ntext'
+    b.description = 'very looooooooooooooooooooooong placeholder\ntext because I want to show that this is a really neat feature'
+    b.descBoxTextSize = b.textSize
     
     buttons = Object.endGroup()
     
     Object.startGroup()
-
-    def bgRed(*args):
-        if args[0].activated:
-            globals.bgColor = color(128,0,0)
-    def bgBlue(*args):
-        if args[0].activated:
-            globals.bgColor = color(0,128,0)
-    def bgGreen(*args):
-        if args[0].activated:
-            globals.bgColor = color(0,0,128)
-    def bgYellow(*args):
-        if args[0].activated:
-            globals.bgColor = color(175, 175, 0)
-    def bgGrey(*args):
-        if args[0].activated:
-            globals.bgColor = color(204)
     
     r *= 0.5
     b1 = Button(width/2, 205 - 80, r.copy()*0.2)
     b1.applyStyle('radio')
     b1.radioGroup = 'testScreen_radiobuttons'
-    b1.releaseAction = bgRed
-    b1.idleAction = bgRed
     
     b1 = Button(width/2, 205 - 40, r.copy()*0.2)
     b1.applyStyle('radio')
     b1.radioGroup = 'testScreen_radiobuttons'
-    b1.releaseAction = bgBlue
-    b1.idleAction = bgBlue
     
     b1 = Button(width/2, 205 + 40, r.copy()*0.2)
     b1.applyStyle('radio')
     b1.radioGroup = 'testScreen_radiobuttons'
-    b1.releaseAction = bgGreen
-    b1.idleAction = bgGreen
     
     b1 = Button(width/2, 205 + 80, r.copy()*0.2)
     b1.applyStyle('radio')
     b1.radioGroup = 'testScreen_radiobuttons'
-    b1.releaseAction = bgYellow
-    b1.idleAction = bgYellow
     
     b1 = Button(width/2, 205, r.copy()*0.2)
     b1.applyStyle('checkbox')
     b1.boxColor = color(255,0,0, 0)
-    b1.idleAction = bgGrey
-    b1.hoverAction = bgGrey
-    b1.pressAction = bgGrey
     
     bGroup2 = Object.endGroup()
 
 def draw():
     scale(globals.baseScale)
-    background(globals.bgColor)
     pushStyle()
     rectMode(CORNER)
     textMode(LEFT)
