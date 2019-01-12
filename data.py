92a53e1bb236463a4601f85a65554e56cbd9350e
Spaalone_Babuguuscooties <0965749@hr.nl>
2019-01-12 23:28:48 +0100

    Overhauled loading system. Returns it's progress when loading assets. This does however require the loadData() function to be looped externally.

--- data.py
+++ data.py
@@ -2,9 +2,45 @@ import os
 import json
 import globals
 
-def loadData():
+loadingAssets = False
+totalRequests = 0
+loadStart = 0
+assetNames = dict()
+def loadData(firstLoad=False):
+    global loadStart
+    global loadingAssets
+    global totalRequests
+    global assetNames
+    
     imgIndex = globals.imgIndex
     fonts = globals.fonts
+    
+    #Wait for all images to finish loading.
+    if firstLoad and not loadingAssets:
+        loadStart = millis()
+    if firstLoad and loadingAssets:
+        # Raise an exception if an asset takes more than 2000 ms to load.
+        # This usually means a memory overflow has ocurred.
+        if millis() - loadStart > 2000:
+            raise BufferError('Asset loading timed out. (>2000ms)')
+        
+        lastLoadedAsset = None
+        finishedRequests = 0
+        for img in imgIndex:
+            imgWidth = imgIndex[img].width
+            if imgWidth == 0:
+                var = False
+            elif imgWidth == -1:
+                var = False
+            else:
+                loadStart = millis()
+                finishedRequests += os.path.getsize(assetNames[img])
+        if totalRequests == finishedRequests:
+            globals.imgIndex = imgIndex
+            loadingAssets = False
+        return totalRequests, finishedRequests
+    else:
+        totalRequests = 0
     
     #Load configuration files
     #Cardconfig
@@ -30,27 +66,18 @@ def loadData():
         
         #Images
         if filetype == "png":
+            totalRequests += os.path.getsize(os.path.realpath('data\\'+file))
             imgIndex[name] = requestImage(file)
+            assetNames[name] = os.path.realpath('data\\'+file)
             
         #Fonts
         elif filetype == "vlw":
             fonts[file.split("-")[0]] = loadFont(file)
-    
-    #Wait for all images to finish loading.
-    while True:
-        var = True
-        for img in imgIndex:
-            imgWidth = imgIndex[img].width
-            if imgWidth == 0:
-                var = False
-            elif imgWidth == -1:
-                var = False
-        if var:
-            break
-        
-    globals.imgIndex = imgIndex
+
+    loadingAssets = True
     globals.cardConfig = cardConfig
     globals.userConfig = userConfig
+    return totalRequests, 0
     
 def saveData():
     userConfig = globals.userConfig
